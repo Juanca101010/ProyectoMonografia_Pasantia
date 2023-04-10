94705a74913b1a47c83b34b0e58adfcd6cd89e85
Juanca101010 <juancristanchom@usantotomas.edu.co>
2023-04-10 16:31:15 -0500

    templates necesarios para la pagina

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('', views.dashboardhw, name='dashboardhw'), 
     path('smsEmail', views.smsEmail, name='smsEmail'), 
     path('loginhw', views.loginhw, name='loginhw'), 
+    path('help', views.help, name='help'), 
+    path('icareplus', views.icareplus, name='icareplus'), 
+    path('netcare', views.netcare, name='netcare'), 
+    path('onebox', views.onebox, name='onebox'), 
+    path('profile', views.profile, name='profile'), 
+    path('vpnaccess', views.vpnaccess, name='vpnaccess'), 
 ]
